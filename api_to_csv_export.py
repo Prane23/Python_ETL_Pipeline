01379fbd35c9cda195f379e0e15223cbb336bad8
Prashant Rane <prashant_rane100@hotmail.com>
2025-09-26 13:14:59 -0700

    additinal atrributes added

--- api_to_csv_export.py
+++ api_to_csv_export.py
@@ -13,26 +13,28 @@ def trabsform(emp, empdetail):
     try:
         emp_df= pd.DataFrame(emp)
         empdetail_df= pd.DataFrame(empdetail)
-        merged_df= pd.merge(emp_df, empdetail_df, left_on='employee_employeeId',right_on='employmentdetail_employeeId',how='inner')
+        #print(empdetail_df)
+        merged_df= pd.merge(emp_df, empdetail_df, on='employeeId',how='inner')
         #print(merged_df)
-        final_df= merged_df[['employee_employeeId','employee_firstName','employee_lastName','employee_email',\
-                             'employmentdetail_jobTitle', 'employmentdetail_department', 'employmentdetail_employmentType']]\
-            .rename(columns={'employee_employeeId': 'EmployeeId','employee_firstName': 'FirstName',\
-                            'employee_lastName': 'LastName','employee_email': 'Email','employmentdetail_jobTitle': 'JobTitle',\
-                            'employmentdetail_department': 'Department','employmentdetail_employmentType': 'EmploymentType'})
+        final_df= merged_df[['employeeId','firstName','lastName','email',\
+                             'jobTitle', 'department', 'employmentType','status','skipManager','hireDate']]\
+            .rename(columns={'employeeId': 'EmployeeId','firstName': 'FirstName',\
+                            'lastName': 'LastName','email': 'Email','jobTitle': 'JobTitle',\
+                            'department': 'Department','employmentType': 'EmploymentType',\
+                            'status': 'Status','skipManager': 'SkipManager','hireDate': 'HireDate'})
     
         #print(final_df)
         return final_df
-    except Exception as err :
-        print(f'error: {err}')
+    except Exception as exp :
+        print(f'Error occured while transforming data: {exp}')
 
 #load function to export data to csv
 def load(final_df):
     try:
         final_df.to_csv('employee_data.csv', index=False)
         print('Data exported to employee_data.csv successfully.')
-    except Exception as err:
-        print(f'error: {err}')
+    except Exception as exp:
+        print(f'Error occured while loading data: {exp}')
 
 empployee = 'https://raw.githubusercontent.com/Prane23/Python_ETL_Pipeline/refs/heads/master/data/employee.json'
 employee_detail= 'https://raw.githubusercontent.com/Prane23/Python_ETL_Pipeline/refs/heads/master/data/employmentdetail.json'
@@ -40,8 +42,8 @@ employee_detail= 'https://raw.githubusercontent.com/Prane23/Python_ETL_Pipeline/
 try :
     emp_response= extract(empployee)
     emp_detail_response= extract(employee_detail)
-    #print(res)
-    # print(emp_detail_response)
+    #print(emp_response)
+    #print(emp_detail_response)
     trabsformed_data= trabsform(json_normalize(emp_response,sep='_'),json_normalize(emp_detail_response,sep='_'))
     load(trabsformed_data)
 except requests.HTTPError as http_error:
